ab78f8e4b7193c8c0d1f7f0c4c4c9f801327a574
salman8896 <salmanshahid8896@gmail.com>
2023-08-08 21:26:09 +0500

    feat: Added a Header component to welcome and Welcome text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { cn } from "../lib/utils";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <div className="text-white">
         <p className={cn("text-white", "p-5 text-lg")}>Hello Sprouts</p>
       </div>
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+export const Header = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center px-60 mt-8">
+        <div className="flex flex-col justify-between items-center h-40 w-full border-white border-2 rounded-3xl py-8">
+          <h2 className=" text-white text-2xl font-bold text-center">
+            Welcome to the Rock Papers and Scissors!
+          </h2>
+          <p className="text-white text-lg font-semibold text-center">
+            Ready Player 1
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
