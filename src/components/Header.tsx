b1ef27617facc464d130dee68facc7afe3492b47
salman8896 <salmanshahid8896@gmail.com>
2023-08-11 01:37:19 +0500

    Added Two Scoreboards

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,15 @@ export const Header = () => {
     <>
       <div className="flex justify-center flex-row px-60 mt-10">
         <div className="flex flex-row justify-between h-40 w-full border-white border-2 rounded-3xl p-5">
-          <h2 className="font-bangers text-white text-4xl font-bold text-left">
+          <div className="flex flex-col bg-white rounded-lg h-28 w-28 mr-5 justify-between items-center py-4">
+            <p className="text-blue-700 text-3xl font-black font-bangers">
+              Score
+            </p>
+            <p className="text-black font-black text-3xl font-bangers">
+              {score}
+            </p>
+          </div>
+          <h2 className="font-bangers text-white text-4xl font-bold text-center">
             Rock <br /> Paper <br /> Scissors
           </h2>
           <div className="flex flex-col bg-white rounded-lg h-28 w-28 mr-5 justify-between items-center py-4">
