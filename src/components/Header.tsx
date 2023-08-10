e2cc165bbcf872c78b654db9e7ee0ab13fd3724b
salman8896 <salmanshahid8896@gmail.com>
2023-08-11 01:25:10 +0500

    Feat: Added Exit Button on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import { useStore } from "../zustand/store";
+import { Button } from "./ui/button";
 
 export const Header = () => {
   const score = useStore((store) => store.score);
   return (
     <>
-      <div className="flex justify-start px-60 mt-10">
+      <div className="flex justify-center flex-row px-60 mt-10">
         <div className="flex flex-row justify-between h-40 w-full border-white border-2 rounded-3xl p-5">
           <h2 className="font-bangers text-white text-4xl font-bold text-left">
             Rock <br /> Paper <br /> Scissors
@@ -18,6 +19,18 @@ export const Header = () => {
             </p>
           </div>
         </div>
+      </div>
+      <div className="relative">
+        <Button
+          variant={"outline"}
+          size={"sm"}
+          className="text-white hover:bg-red-600 hover:border-none absolute right-16 bottom-28 transition-colors duration-500 ease-in-out"
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        >
+          Exit Game
+        </Button>
       </div>
     </>
   );
