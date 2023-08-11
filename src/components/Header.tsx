5e5293cf2736594e8336da0352c4b225f122b898
salman8896 <salmanshahid8896@gmail.com>
2023-08-11 18:33:06 +0500

    Changed Header to conditional render to SinglePlayer & Computer | Multiplayer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,46 +4,88 @@ import { Button } from "./ui/button";
 export const Header = () => {
   const playerMode = useStore((store) => store.playerMode);
   const playerOneScore = useStore((store) => store.playerOneScore);
-  //const playerTwoScore = useStore((store) => store.playerTwoScore);
+  const playerTwoScore = useStore((store) => store.playerTwoScore);
   const robotXScore = useStore((store) => store.robotXScore);
   const robotYScore = useStore((store) => store.robotYScore);
-  return (
-    <>
-      <div className="flex justify-center flex-row px-60 mt-10">
-        <div className="flex flex-row justify-between h-40 w-full border-white border-2 rounded-3xl p-5">
-          <div className="flex flex-col bg-white rounded-lg h-28 w-28 mr-5 justify-between items-center py-4">
-            <p className="text-blue-700 text-3xl font-black font-bangers">
-              Score
-            </p>
-            <p className="text-black font-black text-3xl font-bangers">
-              {playerMode === "singleplayer" ? playerOneScore : robotXScore}
-            </p>
-          </div>
-          <h2 className="font-bangers text-white text-4xl font-bold text-center">
-            Rock <br /> Paper <br /> Scissors
-          </h2>
-          <div className="flex flex-col bg-white rounded-lg h-28 w-28 mr-5 justify-between items-center py-4">
-            <p className="text-blue-700 text-3xl font-black font-bangers">
-              Score
-            </p>
-            <p className="text-black font-black text-3xl font-bangers">
-              {playerMode === "singleplayer" ? robotXScore : robotYScore}
-            </p>
+  if (playerMode === "singleplayer" || playerMode === "computer") {
+    return (
+      <>
+        <div className="flex justify-center flex-row px-60 mt-10">
+          <div className="flex flex-row justify-between h-40 w-full border-white border-2 rounded-3xl p-5">
+            <div className="flex flex-col bg-white rounded-lg h-28 w-28 mr-5 justify-between items-center py-4">
+              <p className="text-blue-700 text-3xl font-black font-bangers">
+                Score
+              </p>
+              <p className="text-black font-black text-3xl font-bangers">
+                {playerMode === "singleplayer" ? playerOneScore : robotXScore}
+              </p>
+            </div>
+            <h2 className="font-bangers text-white text-4xl font-bold text-center">
+              Rock <br /> Paper <br /> Scissors
+            </h2>
+            <div className="flex flex-col bg-white rounded-lg h-28 w-28 mr-5 justify-between items-center py-4">
+              <p className="text-blue-700 text-3xl font-black font-bangers">
+                Score
+              </p>
+              <p className="text-black font-black text-3xl font-bangers">
+                {playerMode === "singleplayer" ? robotXScore : robotYScore}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="relative">
-        <Button
-          variant={"outline"}
-          size={"sm"}
-          className="text-white bg-red-600 hover:border-none hover:bg-red-600/70 absolute right-16 bottom-28 transition-all duration-500 ease-in-out"
-          onClick={() => {
-            window.location.href = "/";
-          }}
-        >
-          Exit Game
-        </Button>
-      </div>
-    </>
-  );
+        <div className="relative">
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            className="text-white bg-red-600 hover:border-none hover:bg-red-600/70 absolute right-16 bottom-28 transition-all duration-500 ease-in-out"
+            onClick={() => {
+              window.location.href = "/";
+            }}
+          >
+            Exit Game
+          </Button>
+        </div>
+      </>
+    );
+  } else if (playerMode === "multiplayer") {
+    return (
+      <>
+        <div className="flex justify-center flex-row px-60 mt-10">
+          <div className="flex flex-row justify-between h-40 w-full border-white border-2 rounded-3xl p-5">
+            <div className="flex flex-col bg-white rounded-lg h-28 w-28 mr-5 justify-between items-center py-4">
+              <p className="text-blue-700 text-3xl font-black font-bangers">
+                Score
+              </p>
+              <p className="text-black font-black text-3xl font-bangers">
+                {playerOneScore}
+              </p>
+            </div>
+            <h2 className="font-bangers text-white text-4xl font-bold text-center">
+              Rock <br /> Paper <br /> Scissors
+            </h2>
+            <div className="flex flex-col bg-white rounded-lg h-28 w-28 mr-5 justify-between items-center py-4">
+              <p className="text-blue-700 text-3xl font-black font-bangers">
+                Score
+              </p>
+              <p className="text-black font-black text-3xl font-bangers">
+                {playerTwoScore}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="relative">
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            className="text-white bg-red-600 hover:border-none hover:bg-red-600/70 absolute right-16 bottom-28 transition-all duration-500 ease-in-out"
+            onClick={() => {
+              window.location.href = "/";
+            }}
+          >
+            Exit Game
+          </Button>
+        </div>
+      </>
+    );
+  }
 };
